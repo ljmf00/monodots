89d422480de37414292c3b6068ddc789537951bd
Luis Ferreira <contact@lsferreira.net>
2025-04-18 00:05:48 +0100

    chore(nix): add support for virtualbox on thinker device

--- nix/hosts/thinker/nixos.nix
+++ nix/hosts/thinker/nixos.nix
@@ -42,7 +42,14 @@
     # Do not set iommu=off, because this will cause the SD-Card reader
     # driver to kernel panic.
     # "iommu=soft"
+
+    # to work with virtual box
+    # https://github.com/NixOS/nixpkgs/issues/363887#issuecomment-2536693220
+    "kvm.enable_virt_at_load=0"
   ];
+
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "luis" ];
 
   services.kubo = {
     enable = true;
