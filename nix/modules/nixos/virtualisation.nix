f4e82e727db5d484515305c1104df40cbcc02438
Luís Ferreira <contact@lsferreira.net>
2025-07-04 15:05:57 +0100

    feat(nix): add support for distrobox

--- nix/modules/nixos/virtualisation.nix
+++ nix/modules/nixos/virtualisation.nix
@@ -3,4 +3,10 @@
 {
   virtualisation.docker.enable = true;
   virtualisation.virtualbox.host.enable = true;
+
+  virtualisation.podman = {
+    enable = true;
+  };
+
+  environment.systemPackages = [ pkgs.distrobox ];
 }
