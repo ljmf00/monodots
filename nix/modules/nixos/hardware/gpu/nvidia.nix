bb86aaed3703c261f08ac19b22fcab02e2753d7c
Luís Ferreira <contact@lsferreira.net>
2025-07-04 15:05:38 +0100

    fix(nix): use better nvidia options and latest package

--- nix/modules/nixos/hardware/gpu/nvidia.nix
+++ nix/modules/nixos/hardware/gpu/nvidia.nix
@@ -30,9 +30,9 @@ in with lib;
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.nvidia = {
-    # forceFullCompositionPipeline = true;
+  hardware.nvidia-container-toolkit.enable = true;
 
+  hardware.nvidia = {
     # Modesetting is required.
     modesetting.enable = true;
 
@@ -40,7 +40,7 @@ in with lib;
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
@@ -52,14 +52,13 @@ in with lib;
     # supported GPUs is at: 
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
+	  # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 }
