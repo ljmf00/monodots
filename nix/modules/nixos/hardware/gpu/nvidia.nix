50c4129e7f29564aa44ff49f1dea3e788cfc9413
Luís Ferreira <contact@lsferreira.net>
2025-08-11 13:56:37 +0100

    chore(nix): disable nvidia container settings

--- nix/modules/nixos/hardware/gpu/nvidia.nix
+++ nix/modules/nixos/hardware/gpu/nvidia.nix
@@ -30,7 +30,7 @@ in with lib;
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.nvidia-container-toolkit.enable = true;
+  # hardware.nvidia-container-toolkit.enable = true;
 
   hardware.nvidia = {
     # Modesetting is required.
