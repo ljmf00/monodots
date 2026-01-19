b3dc54b876f653ecc517b59997a066cad4fb647f
Luís Ferreira <contact@lsferreira.net>
2026-01-19 15:35:25 +0000

    feat(nix): add sunshine server

--- nix/modules/nixos/games.nix
+++ nix/modules/nixos/games.nix
@@ -5,6 +5,23 @@
     enable = true;
   };
 
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+  };
+
+  security.wrappers.sunshine = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_admin+p";
+    source = "${pkgs.sunshine}/bin/sunshine";
+  };
+
+  services.avahi.publish.enable = true;
+  services.avahi.publish.userServices = true;
+
   environment.systemPackages = with pkgs; [
     steamcmd
     steam-run
