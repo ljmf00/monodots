b143c019c26bc15c503febe5e8c2a0b01f4c872e
Luis Ferreira <contact@lsferreira.net>
2026-04-05 12:09:45 +0100

    chore(terraform): add gateway servers

--- terraform/records_servers.tf
+++ terraform/records_servers.tf
@@ -98,6 +98,56 @@ resource "cloudflare_record" "dh_p_de_frkt_01_a_lsferreira" {
   comment = "terraform"
 }
 
+resource "cloudflare_record" "gateway_a_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "gateway.lsferreira.net"
+  value = "198.251.90.61"
+  type = "A"
+  proxied = false
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "ch_gateway_a_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "ch.gateway.lsferreira.net"
+  value = "107.189.6.72"
+  type = "A"
+  proxied = false
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "us1_gateway_a_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "us1.gateway.lsferreira.net"
+  value = "209.141.55.108"
+  type = "A"
+  proxied = false
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "us2_gateway_a_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "us2.gateway.lsferreira.net"
+  value = "198.98.62.203"
+  type = "A"
+  proxied = false
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "us3_gateway_a_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "us3.gateway.lsferreira.net"
+  value = "45.61.188.200"
+  type = "A"
+  proxied = false
+
+  comment = "terraform"
+}
+
 resource "cloudflare_record" "dh_p_de_frkt_01_aaaa_lsferreira" {
   zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
   name = "dh-p-de-frkt-01.internal.lsferreira.net"
