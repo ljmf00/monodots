4156d2cc09f2a348611a1785210236e55604f458
Luis Ferreira <contact@lsferreira.net>
2026-04-10 00:16:15 +0100

    chore(terraform): apply second phase of tf-migrate to migrate from v4 to v5

--- terraform/zone_settings.tf
+++ terraform/zone_settings.tf
@@ -1,49 +1,56 @@
-resource "cloudflare_zone_settings_override" "lsferreira_net_settings" {
-  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+# tf-migrate: resource "cloudflare_zone_settings_override" "lsferreira_net_settings" {
+# tf-migrate:   zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
 
-  settings {
-    # SSL/TLS
-    ssl             = "strict"
-    tls_1_3         = "zrt"
-    min_tls_version = "1.3"
+# tf-migrate:   settings {
+# tf-migrate:     # SSL/TLS
+# tf-migrate:     ssl             = "strict"
+# tf-migrate:     tls_1_3         = "zrt"
+# tf-migrate:     min_tls_version = "1.3"
 
-    # HTTP(S)
-    always_use_https         = "on"
-    automatic_https_rewrites = "on"
-    opportunistic_encryption = "on"
-    /* http2 = "on" # read-only */
-    http3 = "on"
+# tf-migrate:     # HTTP(S)
+# tf-migrate:     always_use_https         = "on"
+# tf-migrate:     automatic_https_rewrites = "on"
+# tf-migrate:     opportunistic_encryption = "on"
+# tf-migrate:     /* http2 = "on" # read-only */
+# tf-migrate:     http3 = "on"
 
-    # Network
-    zero_rtt   = "on"
-    websockets = "on"
+# tf-migrate:     # Network
+# tf-migrate:     zero_rtt   = "on"
+# tf-migrate:     websockets = "on"
 
-    # IPv4/IPv6
-    pseudo_ipv4 = "off"
-    ipv6        = "on"
+# tf-migrate:     # IPv4/IPv6
+# tf-migrate:     pseudo_ipv4 = "off"
+# tf-migrate:     ipv6        = "on"
 
-    # Tor
-    opportunistic_onion = "on"
+# tf-migrate:     # Tor
+# tf-migrate:     opportunistic_onion = "on"
 
-    # Caching
-    development_mode  = "off"
-    cache_level       = "basic"
-    browser_cache_ttl = 14400
-    rocket_loader     = "off"
-    minify {
-      css  = "on"
-      js   = "on"
-      html = "on"
-    }
+# tf-migrate:     # Caching
+# tf-migrate:     development_mode  = "off"
+# tf-migrate:     cache_level       = "basic"
+# tf-migrate:     browser_cache_ttl = 14400
+# tf-migrate:     rocket_loader     = "off"
+# tf-migrate:     minify {
+# tf-migrate:       css  = "on"
+# tf-migrate:       js   = "on"
+# tf-migrate:       html = "on"
+# tf-migrate:     }
 
-    # Compression
-    brotli = "on"
+# tf-migrate:     # Compression
+# tf-migrate:     brotli = "on"
 
-    # Security
-    security_level = "high"
-    challenge_ttl  = 86400
-    security_header {
-      enabled = true
-    }
+# tf-migrate:     # Security
+# tf-migrate:     security_level = "high"
+# tf-migrate:     challenge_ttl  = 86400
+# tf-migrate:     security_header {
+# tf-migrate:       enabled = true
+# tf-migrate:     }
+# tf-migrate:   }
+# tf-migrate: }
+
+removed {
+  from = cloudflare_zone_settings_override.lsferreira_net_settings
+  lifecycle {
+    destroy = false
   }
 }
