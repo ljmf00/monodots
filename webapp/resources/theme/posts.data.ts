b13d5b3379c2dee5eeec4bb35ffcdea0364ec63b
Luís Ferreira <contact@lsferreira.net>
2026-06-12 11:57:27 +0100

    chore(treewide): remove stale webapp

--- webapp/resources/theme/posts.data.ts
+++
@@ -1,8 +0,0 @@
-import { type PostMetadata, createPostsLoader } from './src/posts'
-
-// data reactive object
-declare const data: PostMetadata[]
-export      { data }
-
-// post loader
-export default createPostsLoader()
